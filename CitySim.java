92aa806c1415f03b2961dbc0155f78344095da00
William Kurek <wrk10@pitt.edu>
2017-02-20 18:31:05 -0500

    Update CitySim.java

--- CitySim.java
+++ CitySim.java
@@ -6,19 +6,24 @@ public class CitySim {
 
     private final Random random;
     int sennottFrequency;
+    public static String travelSummary;
 
     public CitySim(Random rand) {
         this.random = rand;
     }
 
     public static void travelSummary(Driver driver, Location start, Route route, Location end) {
-        System.out.println(driver.toString()
+        travelSummary = (driver.toString()
                 + " heading from "
                 + start.toString()
                 + " to "
                 + end.toString()
                 + " via "
                 + route.toString());
+    }
+
+    public static String getTravelSummary() {
+        return travelSummary;
     }
 
     public Location getStartLocation() {
